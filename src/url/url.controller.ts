4d86ed30375e1ead8ea56fcf2446ee353f82a8bb
erensarac <erenkansarac@hotmail.com>
2025-05-14 03:05:40 +0300

    feat(url): update url controller to handle get and create urls
    - Add get endpoint for short url redirect
    
    - Add create short url endpoint for creating short urls
    
    - Add access count increment logic to track usage

--- src/url/url.controller.ts
+++ src/url/url.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Res, Redirect, } from '@nestjs/common';
 import { UrlService } from './url.service';
+import { ShortUrl as UrlModel } from 'generated/prisma/client';
+import { Response } from 'express';
 
 @Controller('url')
 export class UrlController {
   constructor(private readonly urlService: UrlService) {}
 
-  @Get()
-  getHello(): string {
-    return this.urlService.getUrl();
+  @Get('/:code')
+  async getShortenURL(@Param('code') code: string, @Res() res: Response) {
+    const data = await this.urlService.getShortenURL({ code });
+
+    if (data) {
+      return res.status(302).redirect(data.original)
+    }
+
+    return res.status(404).send('Not found')
+  }
+
+  @Post()
+  async createURL(@Body() data: { original: string }): Promise<UrlModel> {
+    const code = await this.urlService.createUniqueCode();
+
+    return this.urlService.createShortenURL({ ...data, code });
   }
 }
