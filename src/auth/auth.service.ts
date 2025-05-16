51f01efcb068ebfe55da49e14ca0becf1517ce4a
erensarac <erenkansarac@hotmail.com>
2025-05-16 23:08:10 +0300

    feat(auth): add validatedUser and login methods for authentication

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,14 +3,48 @@ import {
   ConflictException,
   Injectable,
   InternalServerErrorException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 import { Prisma } from 'generated/prisma/client';
 import * as bcrypt from 'bcrypt';
+import { ValidatedUser } from './auth.type';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(username: string, password: string): Promise<ValidatedUser | null> {
+    const user = await this.usersService.findUser(username);
+
+    if (user) {
+      try {
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (passwordMatch) {
+          return { id: user.id, username: user.username };
+        }
+
+        throw new UnauthorizedException('Invalid password');
+      } catch (err) {
+        throw err || new InternalServerErrorException();
+      }
+    }
+
+    return null;
+  }
+
+  async login(user: Prisma.UserWhereUniqueInput) {
+    const payload = { username: user.username, sub: user.id };
+
+    return {
+      accessToken: this.jwtService.sign(payload),
+    };
+  }
 
   async register(data: Prisma.UserCreateInput) {
     if (!data.username || !data.password) {
