5629910c93c668262bb7cbebcddeff50ddde23f0
erensarac <erenkansarac@hotmail.com>
2025-05-16 23:06:45 +0300

    feat(auth): add JwtStrategy, LocalStrategy, ValidatedUser type and auth constants

---
+++ src/auth/local.strategy.ts
@@ -0,0 +1,22 @@
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { ValidatedUser } from './auth.type';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super();
+  }
+
+  async validate(username: string, password: string): Promise<ValidatedUser> {
+    const user = await this.authService.validateUser(username, password);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
