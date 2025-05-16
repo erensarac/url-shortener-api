97205013661259f729211ef7fe53862d6cb78997
erensarac <erenkansarac@hotmail.com>
2025-05-17 02:02:13 +0300

    feat(auth): add AuthenticatedRequest interface extending Request type

--- src/auth/auth.type.ts
+++ src/auth/auth.type.ts
@@ -1,3 +1,7 @@
 import { User } from 'generated/prisma/client';
 
 export type ValidatedUser = Pick<User, 'id' | 'username'>;
+
+export interface AuthenticatedRequest extends Request {
+  user: ValidatedUser;
+}
